511a5e085c6a15b1d6e6b3b418d32c8e47541064
jocelynalsdorf <jfrodsla@gmail.com>
2015-04-25 10:27:23 -0700

    changes method so that it provides anagrams from a list of words

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,21 @@
 
 
-class String
-  define_method(:anagram) do |list|
-    word = self
-  end
+class String 
+define_method(:ana) do |list|
+word = self.split("").sort()
+converted = list.split(" ")
+empty = []
+final = []
+i = 0
+converted.each() do |item|
+	empty.push(item.split("").sort())     
+	if empty[i].==(word)
+	     final.push(item)
+	     i += 1
+	    else
+	    	i += 1
+	    end 
+	     end
+puts "Your anagrams are: " + final.join(" "+ "and" + " ")
+end
 end
